9346d6e530163d2d892ea7276929707a25648ce8
Anna Dinh <Annathuydinh@gmail.com>
2020-10-07 14:41:52 -0700

    Completion of valid anagram

---
+++ Easy Leetcode Top Interview Questions/Strings/ValidAnagram.js
@@ -0,0 +1,44 @@
+/*
+Given two strings s and t , write a function to determine if t is an anagram of s.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+Note:
+You may assume the string contains only lowercase alphabets.
+*/
+
+var isAnagram = function(s, t) {
+  if (s.length !== t.length || (s.length === 0 || t.length === 0)) {
+    return false;
+  }
+  t = t.split("");
+  for (let i = 0; i < s.length; i++) {
+    let current = t.indexOf(s[i]);
+    if (current > -1) {
+      t.splice(current, 1);
+    }
+  }
+  if (t.length === 0) {
+    return true
+  } else {
+    return false
+  }
+
+};
+
+let example1 = isAnagram("anagram", "nagaram"); // true
+let example2 = isAnagram("hello", "elloh"); // true
+let example3 = isAnagram("what", "cool"); // false
+let example4 = isAnagram("hi", "hey"); // false
+let example5 = isAnagram("", ""); // false
+console.log(example1);
+console.log(example2);
+console.log(example3);
+console.log(example4);
+console.log(example5);
